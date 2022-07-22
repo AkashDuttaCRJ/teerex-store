71c76a312a745a021d1598010fbf212afb6613d4
AkashDuttaCRJ <akashduttaofficial2503@gmail.com>
2022-07-22 09:19:01 +0530

    Added search functionality and media queries

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
   useEffect(() => {
     const getData = async () => {
       const resp = await (await fetch(url)).json()
-      console.log(resp);
       setResponse(resp)
       setData(resp)
     }
--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -9,6 +9,8 @@ const Products = ({ data, setData, originalData }) => {
     const [priceFilter, setPriceFilter] = useState(null)
     const [genderFilter, setGenderFilter] = useState(null)
     const [loading, setLoading] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('')
+    const [showMobileFilter, setShowMobileFilter] = useState(false)
 
     const getColour = (dataset) => {
         const colors = []
@@ -71,13 +73,38 @@ const Products = ({ data, setData, originalData }) => {
 
         setFilterOptions(filters);
         setData(originalData);
+        const closeModal = () => {
+            setShowMobileFilter(false)
+        }
+        document.addEventListener('click', closeModal)
+
+        return () => document.removeEventListener('click', closeModal)
         // eslint-disable-next-line
     },[])
+
+    useEffect(() => {
+      searchTerm === '' && setData(originalData)
+      // eslint-disable-next-line
+    }, [searchTerm])
+    
 
     const filterResults = () => {
         setLoading(true)
         // filter data based on filter options
-        const filteredData = originalData.filter(item => {
+        const filteredData = searchTerm === '' ? originalData.filter(item => {
+            if (typeFilter && item.type !== typeFilter) {
+                return false
+            }
+            if (colorFilter && item.color !== colorFilter) {
+                return false
+            }
+            if (priceFilter && ((item.price < priceFilter?.minValue) || (item.price > priceFilter?.maxValue))) {
+                return false
+            }
+            if (genderFilter && item.gender !== genderFilter) {
+                return false
+            } else return item
+        }) : data.filter(item => {
             if (typeFilter && item.type !== typeFilter) {
                 return false
             }
@@ -95,11 +122,85 @@ const Products = ({ data, setData, originalData }) => {
         setLoading(false)
     }
 
-    console.log(typeFilter, colorFilter, priceFilter, genderFilter);
+    const getSearchResult = () => {
+        const searchResults = originalData.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()))
+        setData(searchResults)
+    }
 
   return (
     <div className='products-page'>
-        <div className="search-container"></div>
+        <div className="search-container">
+            <div className="search-input-container">
+                <input 
+                    type="text" 
+                    placeholder="Search for products..." 
+                    className="search-input" 
+                    value={searchTerm} 
+                    onChange={(e) => setSearchTerm(e.target.value)} 
+                    onKeyUp={(e) => e.key === 'Enter' && getSearchResult()} 
+                />
+                {searchTerm !== '' && <button className="cross-button" onClick={() => setSearchTerm('')}>
+                    <img src='assets/bx-x.svg' alt='' />
+                </button>}
+                <button className="search-button" onClick={getSearchResult}>
+                    <img src='assets/bx-search.svg' alt='' />
+                </button>
+                <button className="filter-button mobile" onClick={(e) => {
+                    e.stopPropagation()
+                    setShowMobileFilter(!showMobileFilter)
+                    }}>
+                    <img src='assets/bx-filter-alt.svg' alt='' />
+                </button>
+                {showMobileFilter && <div className='mobile-filters-container' onClick={(e) => e.stopPropagation()}>
+                    <div className='filter-header'>
+                        <div className='filter-header-label'>Filters</div>
+                        <div className='filter-clear-btn' onClick={() => {
+                            setTypeFilter(null)
+                            setColorFilter(null)
+                            setPriceFilter(null)
+                            setGenderFilter(null)
+                            setData(originalData)
+                            setSearchTerm('')
+                            setShowMobileFilter(false)
+                        }}>Clear all</div>
+                    </div>
+                    {filterOptions.map((item, index) => (
+                        <div className='filter-group' key={index}>
+                            <div className="filter-label">{item.label}</div>
+                            {item.values.map((value, index) => (
+                                item.label === 'Price' ?
+                                <div className="filter-mobile-item">
+                                    <label htmlFor={JSON.stringify(value)}>
+                                        <input type='radio' value={JSON.stringify(value)} checked={JSON.stringify(priceFilter) === JSON.stringify(value)} id={JSON.stringify(value)} name={item.label} onChange={((e) => setPriceFilter(JSON.parse(e.target.value)))} />
+                                        <div className="filter-mobile-item-label">{`Rs${value.minValue} - Rs${value.maxValue}`}</div>
+                                    </label>
+                                </div> :
+                                <div className="filter-mobile-item">
+                                    <label htmlFor={value}>
+                                        <input 
+                                        type='radio' 
+                                        value={value} 
+                                        id={value} 
+                                        name={item.label} 
+                                        checked={(item.label === 'Type' && (typeFilter === value)) || (item.label === 'Colour' && (colorFilter === value)) || (item.label === 'Gender' && (genderFilter === value))}
+                                        onChange={(e) => {
+                                            if (item.label === 'Type') setTypeFilter(e.target.value)
+                                            if (item.label === 'Colour') setColorFilter(e.target.value)
+                                            if (item.label === 'Gender') setGenderFilter(e.target.value)
+                                        }} />
+                                        <div className="filter-mobile-item-label">{value}</div>
+                                    </label>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                    <button className='apply-filter-btn' onClick={() => {
+                        filterResults()
+                        setShowMobileFilter(false)
+                    }} disabled={!typeFilter && !colorFilter && !priceFilter && !genderFilter}>Apply Filter</button>
+                </div>}
+            </div>
+        </div>
         <div className="results-container">
             <div className="filter-container">
                 <div className='filter-header'>
@@ -110,6 +211,7 @@ const Products = ({ data, setData, originalData }) => {
                         setPriceFilter(null)
                         setGenderFilter(null)
                         setData(originalData)
+                        setSearchTerm('')
                     }}>Clear all</div>
                 </div>
                 {filterOptions.map((item, index) => (
